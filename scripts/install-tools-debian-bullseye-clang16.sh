82083bac138e1f702a6b45522702ebefe292ed4d
GoYamada <go@MacBook-Pro-M3.local>
2024-10-15 21:51:35 +0900

    更新

--- scripts/install-tools-debian-bullseye-clang16.sh
+++ scripts/install-tools-debian-bullseye-clang16.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 
-echo install clangd
+echo "Installing clangd"
 
-#apt -y install afl-clang
-
+# 依存パッケージをインストール
 apt -y install wget gnupg
 
-# debian
+# LLVM リポジトリを追加
 echo 'deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-16 main' >> /etc/apt/sources.list
 echo 'deb-src http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-16 main' >> /etc/apt/sources.list
 
+# GPG キーの追加
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 
-apt -y update
+# パッケージリストの更新
+apt-get update --fix-missing
 
-apt -y install clang-16
+# clang 16 のインストール
+apt-get -y install clang-16 || echo "clang-16 installation failed"
 
+# clang のパスを設定
 update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 1
 update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 1
